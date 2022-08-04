8e1e5ab3a268a5476c16391f8541c4b2548c11b2
ashujavaaditya <ashujavaaditya@gmail.com>
2022-08-04 13:01:24 +0530

    First Commit

---
+++ src/main/java/com/nicsi/JavaJenkinsApplication.java
@@ -0,0 +1,27 @@
+package com.nicsi;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJenkinsApplication 
+{
+
+	public static Logger logger = LoggerFactory.getLogger(JavaJenkinsApplication.class);
+	
+	@PostConstruct
+	public void intt()
+	{
+		logger.info("Application Started...");
+	}
+	public static void main(String[] args) 
+	{
+		logger.info("Application Executed...");
+		SpringApplication.run(JavaJenkinsApplication.class, args);
+	}
+
+}
